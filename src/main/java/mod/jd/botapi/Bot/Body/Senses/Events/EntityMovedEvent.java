e6db764aec8ae7e99cf18fadbdc1950720c23edb
Jaideep Singh Heer <jaideesinghheer@gmail.com>
2017-05-30 10:09:03 +0530

    Completed and documented PlayerBody. Used KeyBindings Forced PlayerBody Constructor to take an argument of EntityPlayerSP.

--- src/main/java/mod/jd/botapi/Bot/Body/Senses/Events/EntityMovedEvent.java
+++
@@ -1,9 +0,0 @@
-package mod.jd.botapi.Bot.Body.Senses.Events;
-
-import net.minecraftforge.fml.common.eventhandler.Event;
-
-/**
- * To be fired when the entity moves.
- */
-public class EntityMovedEvent extends Event{
-}
