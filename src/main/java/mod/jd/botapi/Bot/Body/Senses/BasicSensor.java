7300c212f360240db45c0b25876535b9d82f6770
Jaideep Singh Heer <jaideesinghheer@gmail.com>
2017-06-18 17:38:04 +0530

    Update copyright.

--- src/main/java/mod/jd/botapi/Bot/Body/Senses/BasicSensor.java
+++ src/main/java/mod/jd/botapi/Bot/Body/Senses/BasicSensor.java
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package mod.jd.botapi.Bot.Body.Senses;
 
 import net.minecraft.block.Block;
--- src/main/java/mod/jd/botapi/Commands/botCommand.java
+++ src/main/java/mod/jd/botapi/Commands/botCommand.java
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package mod.jd.botapi.Commands;
 import mod.jd.botapi.BotAPI;
 import net.minecraft.command.CommandException;
